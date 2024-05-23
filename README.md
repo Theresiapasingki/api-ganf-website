2e2ac1b5a896c58668477b3afa2841b1f56f8bf7
Rachma Adzima Maulida <rachmadzima4@gmail.com>
2024-05-23 13:03:15 +0700

    Completed all modules and APIs for the GANF Website

---
+++ README.md
@@ -0,0 +1,5 @@
+# GANF Website - Express.js with Firebase
+
+This project integrates Firebase with an Express.js application for the GANF Website. The project includes basic authentication, Firestore database usage, and Firebase Storage for handling media files.
+
+API URL : https://ganf-backend.vercel.app
---
+++ src/config/firebase.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { initializeApp } = require('firebase/app');
+const { getFirestore } = require('firebase/firestore');
+const { getAuth } = require('firebase/auth');
+const { getStorage } = require('firebase/storage');
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Get services
+const auth = getAuth(app);
+const database = getFirestore(app);
+const storage = getStorage(app);
+
+module.exports = {
+  auth,
+  database,
+  storage,
+};
